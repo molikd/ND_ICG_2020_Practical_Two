ac68b1e4e6b7d04d9884a2a52f5db287495d2138
Nitin Vincent <nvincen2@crcfe01.crc.nd.edu>
2020-08-25 12:02:55 -0400

    changes made - practical done

--- practical.sh
+++ practical.sh
@@ -13,4 +13,21 @@ trimmomatic PE -threads 8 SRR2584863_1.fastq.gz SRR2584863_2.fastq.gz \
 	SRR2584863_2.trim.fastq SRR2584863_2.untrim.fastq \
 	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 
-velveth Assem 31 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq
+trimmomatic PE -threads 8 SRR2584866_1.fastq.gz SRR2584866_2.fastq.gz \
+        SRR2584866_1.trim.fastq SRR2584866_1.untrim.fastq \
+        SRR2584866_2.trim.fastq SRR2584866_2.untrim.fastq \
+        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+trimmomatic PE -threads 8 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
+        SRR2589044_1.trim.fastq SRR2589044_1.untrim.fastq \
+        SRR2589044_2.trim.fastq SRR2589044_2.untrim.fastq \
+        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+velveth Assem 27 -fastq -separate -shortPaired  SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq \
+-shortPaired2 SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
+-shortPaired3 SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
+
+velvetg Assem -cov_cutoff 5 -min_contig_lgth 100
+
+tail -1 Assem/Log
+
