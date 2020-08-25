59f0b4c917e025ce8b10962d04d0df819d001dd7
Seth VanDexter <svandext@crcfe02.crc.nd.edu>
2020-08-24 21:15:44 -0400

    svandext first commit

--- practical.sh
+++ practical.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -M <you>@nd.edu
+#$ -M svandext@nd.edu
 #$ -m abe
 #$ -pe smp 8
 #$ -N BIOS60132_Practical_Two
@@ -13,4 +13,22 @@ trimmomatic PE -threads 8 SRR2584863_1.fastq.gz SRR2584863_2.fastq.gz \
 	SRR2584863_2.trim.fastq SRR2584863_2.untrim.fastq \
 	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 
-velveth Assem 31 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq
+trimmomatic PE -threads 8 SRR2584866_1.fastq.gz SRR2584866_2.fastq.gz \
+        SRR2584866_1.trim.fastq SRR2584866_1.untrim.fastq \
+        SRR2584866_2.trim.fastq SRR2584866_2.untrim.fastq \
+        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+trimmomatic PE -threads 8 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
+        SRR2589044_1.trim.fastq SRR2589044_1.untrim.fastq \
+        SRR2589044_2.trim.fastq SRR2589044_2.untrim.fastq \
+        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+velveth Assem 27 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq \
+	SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
+	SRR2589044_1.trim.fastq SRR2580466.trim.fastq
+
+mkdir velvetg_out
+
+./velvetg /afs/crc.nd.edu/user/s/svandext/Private/ND_ICG_2020_Practical_Two/velvetg_out -min_cov 5 -min_contig_lgth 50
+
+
