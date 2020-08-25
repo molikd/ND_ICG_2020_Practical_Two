f955dfb0733b4a5d95b8bd0123ca41a5d8b0d214
Meghana Machireddy <mmachire@crcfe01.crc.nd.edu>
2020-08-25 01:40:53 -0400

    submit assignment

--- practical.sh
+++ practical.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#$ -M <you>@nd.edu
+ #!/bin/bash
+#$ -M mmachire@nd.edu
 #$ -m abe
 #$ -pe smp 8
 #$ -N BIOS60132_Practical_Two
@@ -13,4 +13,19 @@ trimmomatic PE -threads 8 SRR2584863_1.fastq.gz SRR2584863_2.fastq.gz \
 	SRR2584863_2.trim.fastq SRR2584863_2.untrim.fastq \
 	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 
-velveth Assem 31 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq
+trimmomatic PE -threads 8 SRR2584866_1.fastq.gz SRR2584866_2.fastq.gz \
+	SRR2584866_1.trim.fastq SRR2589866_1.untrim.fastq \
+	SRR2584866_2.trim.fastq SRR2589866_2.untrim.fastq \
+	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+trimmomatic PE -threads 8 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
+	SRR2589044_1.trim.fastq SRR2589044_1.untrim.fastq \
+	SRR2589044_2.trim.fastq SRR2589044_2.untrim.fastq \
+	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINSCLIP:NexteraPE-PE.fa:2:40:15
+
+
+velveth Assem 27 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
+
+velvetg -min_contid_lgth 100 -exp_cov 5.0 
+
+less Assem/stats.txt 
