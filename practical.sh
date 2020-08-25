dbccc69b01b09c5470dd78e1eacfdac9c6fd08e1
Mae <merimaat1@gmail.com>
2020-08-25 04:20:40 -0400

    Simplified and updated final solution

--- practical.sh
+++ practical.sh
@@ -16,12 +16,14 @@ for f in $readList; do
 		"$f"_1.trim.fastq "$f"_1.untrim.fastq \
 		"$f"_2.trim.fastq "$f"_2.untrim.fastq \
 		SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+
+	#Combine trimmed fastq files of each read mate
+	cat "$f"_1.trim.fastq >> SRRCombined_1.trim.fastq
+	cat "$f"_2.trim.fastq >> SRRCombined_2.trim.fastq
 done
 
 #Run velvet on all trimmed fastq data using a k-mer (hash) length of 27
-velveth Assem 27 -fastq -separate -shortPaired SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq \
-	-shortPaired2 SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
-	-shortPaired3 SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
+velveth Assem 27 -fastq -separate -shortPaired SRRCombined_1.trim.fastq SRRCombined_2.trim.fastq
 
 #Run  velvetg to only use sequences with a coverage of 5 or more,
 # as well as only contigs with a length of 100 or more
@@ -29,3 +31,6 @@ velvetg Assem -cov_cutoff 5 -min_contig_lgth 100
 
 #Output the contig N50 of the resulting assembly
 grep "n50" Assem/Log
+
+#Clean up for subsequent script usage
+rm SRRCombined_*.trim.fastq
