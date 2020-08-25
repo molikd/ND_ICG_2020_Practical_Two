b1dcd4d0eb3c6d5753a527cd4fb296d81410add4
Mae <merimaat1@gmail.com>
2020-08-24 20:40:28 -0400

    Initial solutions to be tested

--- practical.sh
+++ practical.sh
@@ -8,9 +8,29 @@ module load bio/2.0
 
 fastqc -t 8 *.gz
 
-trimmomatic PE -threads 8 SRR2584863_1.fastq.gz SRR2584863_2.fastq.gz \
-	SRR2584863_1.trim.fastq SRR2584863_1.untrim.fastq \
-	SRR2584863_2.trim.fastq SRR2584863_2.untrim.fastq \
-	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+#Make list of specified paired-end reads
+readList="SRR2584863 SRR2584866 SRR2589044"
+for f in $readList; do
+	#Trim specified reads
+	trimmomatic PE -threads 8 "$f"_1.fastq.gz "$f"_2.fastq.gz \
+		"$f"_1.trim.fastq "$f"_1.untrim.fastq \
+		"$f"_2.trim.fastq "$f"_2.untrim.fastq \
+		SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 
-velveth Assem 31 -shortPaired -fastq -separate SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq
+	#Combine trimmed fastq files of each read mate
+	cat "$f"_1.trim.fastq >> SRRCombined_1.trim.fastq
+	cat "$f"_2.trim.fastq >> SRRCombined_2.trim.fastq
+done
+
+#Run velvet on all trimmed fastq data using a k-mer (hash) length of 27
+velveth Assem 27 -shortPaired -fastq -separate SRRCombined_1.trim.fastq SRRCombined_2.trim.fastq
+
+#Run  velvetg to only use sequences with a coverage of 5 or more,
+# as well as only contigs with a length of 100 or more
+velvetg Assem -cov_cutoff 5 -min_contig_lgth 100
+
+#Output the contig N50 of the resulting assembly
+grep "n50" Assem/stats.txt
+
+#Clean up
+rm SRRCombined_*.trim.fastq
