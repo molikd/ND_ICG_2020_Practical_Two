784bb126b5e08215cc664dfbc182472bff1fdc1d
Amanda Driscoe <adriscoe@crcfe01.crc.nd.edu>
2020-09-19 14:32:02 -0400

    Finished Practical 2

--- practical.sh
+++ practical.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
-#$ -M <you>@nd.edu
+#$ -M adriscoe@nd.edu
 #$ -m abe
 #$ -pe smp 8
 #$ -N BIOS60132_Practical_Two
 
 module load bio/2.0
 
+# Quality Control
 fastqc -t 8 *.gz
 
-trimmomatic PE -threads 8 SRR2584863_1.fastq.gz SRR2584863_2.fastq.gz \
-	SRR2584863_1.trim.fastq SRR2584863_1.untrim.fastq \
-	SRR2584863_2.trim.fastq SRR2584863_2.untrim.fastq \
-	SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+# Trimming: For Loop to use trimmomatic on each of the PE fastq.gz files
+for infile in *_1.fastq.gz
+do
+	base=$(basename ${infile} _1.fastq.gz)
+	trimmomatic PE ${infile} ${base}_2.fastq.gz \
+        	${base}_1.trim.fastq.gz ${base}_1un.trim.fastq.gz \
+                ${base}_2.trim.fastq.gz ${base}_2un.trim.fastq.gz \
+                SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
+done
 
-trimmomatic PE -threads 8 SRR2584866_1.fastq.gz SRR2584866_2.fastq.gz \
-        SRR2584866_1.trim.fastq SRR2584866_1.untrim.fastq \
-        SRR2584866_2.trim.fastq SRR2584866_2.untrim.fastq \
-        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+echo -e "\nFinished Trimmomatic\n"
 
-trimmomatic PE -threads 8 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
-        SRR2589044_1.trim.fastq SRR2589044_1.untrim.fastq \
-        SRR2589044_2.trim.fastq SRR2589044_2.untrim.fastq \
-        SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+# Combine all trimmed fastq files by read direction
+cat *_1.trim.fastq.gz > forward.trim.all.fastq.gz
+cat *_2.trim.fastq.gz >	reverse.trim.all.fastq.gz
 
-velveth Assem 27 -fastq -separate -shortPaired  SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq \
--shortPaired2 SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
--shortPaired3 SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
+echo -e "\nCombined Trimmed fastq files\n"
 
+# Genome assembly: velveth, set k-mer length to 27
+velveth Assem 27 -fastq.gz -separate -shortPaired forward.trim.all.fastq.gz reverse.trim.all.fastq.gz
+ 
+echo -e "\nFinished velveth\n"
+
+# velvetg:  Only use sequences with a coverage of 5 or more and minimum contig length of 100
 velvetg Assem -cov_cutoff 5 -min_contig_lgth 100
 
+echo -e "\nFinished velvetg\n"
+
+# Output the contig N50 of the resulting assembly
 tail -1 Assem/Log
+
+echo -e "\nThe End\n"
 
