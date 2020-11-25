13fc9a07cb230f85982617cbca1918171dcae978
msievert <msievert@nd.edu>
2020-11-25 13:31:15 -0500

    added

--- practical.sh
+++ practical.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#$ -M <you>@nd.edu
+#$ -M msievert@nd.edu
 #$ -m abe
 #$ -pe smp 8
-#$ -N BIOS60132_Practical_Two
+#$ -N pac2
 
 module load bio/2.0
 
@@ -24,8 +24,8 @@ trimmomatic PE -threads 8 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
         SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 
 velveth Assem 27 -fastq -separate -shortPaired  SRR2584863_1.trim.fastq SRR2584863_2.trim.fastq \
--shortPaired2 SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
--shortPaired3 SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
+	 SRR2584866_1.trim.fastq SRR2584866_2.trim.fastq \
+	 SRR2589044_1.trim.fastq SRR2589044_2.trim.fastq
 
 velvetg Assem -cov_cutoff 5 -min_contig_lgth 100
 
